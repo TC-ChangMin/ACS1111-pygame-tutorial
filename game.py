3b7ce859444b670476ff401090c09d3c034ca2d7
Nick Nguyen <nickmn2006@gmail.com>
2024-11-11 19:56:20 -0800

    checked solutions for part5

--- game.py
+++ game.py
@@ -1,19 +1,17 @@
-# Example 2
-
-# Import and initialize pygame
 import pygame
-from random import randint
+from random import randint, choice
 pygame.init()
-
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
-clock = pygame.time.Clock()
 
+# Lanes
+lanes = [93, 218, 343]
+
+# ----------------------------------------------
+# Game Object
 class GameObject(pygame.sprite.Sprite):
   def __init__(self, x, y, image):
     super(GameObject, self).__init__()
-    # self.surf = pygame.Surface((width, height))
-    # self.surf.fill((255, 0, 255))
     self.surf = pygame.image.load(image)
     self.x = x
     self.y = y
@@ -21,24 +19,47 @@ class GameObject(pygame.sprite.Sprite):
   def render(self, screen):
     screen.blit(self.surf, (self.x, self.y))
 
+# ----------------------------------------------
+# Apple
 class Apple(GameObject):
   def __init__(self):
     super(Apple, self).__init__(0, 0, './images/apple.png')
     self.dx = 0
     self.dy = (randint(0, 200) / 100) + 1
-    self.reset() # call reset here! 
+    self.reset()
 
   def move(self):
     self.x += self.dx
     self.y += self.dy
-    # Check the y position of the apple
-    if self.y > 500: 
+    if self.y > 500:
       self.reset()
 
-# add a new method
   def reset(self):
-    self.x = randint(50, 400)
+    self.x = choice(lanes)
     self.y = -64
+
+# ----------------------------------------------
+# Strawberry
+class Strawberry(GameObject):
+  def __init__(self):
+    super(Strawberry, self).__init__(0, 0, './images/strawberry.png')
+    self.dx = (randint(0, 200) / 100) + 1
+    self.dy = 0
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.x > 500:
+      self.reset()
+
+  def reset(self):
+    self.x = -64
+    self.y = choice(lanes)
+
+
+# -------------------------------------------
+# Player
 
 
 class Player(GameObject):
@@ -46,34 +67,71 @@ class Player(GameObject):
     super(Player, self).__init__(0, 0, './images/player.png')
     self.dx = 0
     self.dy = 0
+    self.pos_x = 1
+    self.pos_y = 1
     self.reset()
 
   def left(self):
-    self.dx -= 100
+    if self.pos_x > 0:
+      self.pos_x -= 1
+    self.update_dx_dy()
 
   def right(self):
-    self.dx += 100
+    if self.pos_x < len(lanes) - 1:
+      self.pos_x += 1
+    self.update_dx_dy()
 
   def up(self):
-    self.dy -= 100
+    if self.pos_y > 0:
+      self.pos_y -= 1
+    self.update_dx_dy()
 
   def down(self):
-    self.dy += 100
+    if self.pos_y < len(lanes) - 1:
+      self.pos_y += 1
+    self.update_dx_dy()
 
   def move(self):
     self.x -= (self.x - self.dx) * 0.25
     self.y -= (self.y - self.dy) * 0.25
 
   def reset(self):
-    self.x = 250 - 32
-    self.y = 250 - 32
+    self.x = lanes[self.pos_x]
+    self.y = lanes[self.pos_y]
+    self.dx = self.x
+    self.dy = self.y
+  
+  def update_dx_dy(self):
+    self.dx = lanes[self.pos_x]
+    self.dy = lanes[self.pos_y]
 
+
+# ---------------------------------------
+# make instances 
+
+# Make a group
+all_sprites = pygame.sprite.Group()
+
+# Make Fruit instances
 apple = Apple()
+strawberry = Strawberry()
+
+# make instance of Player
 player = Player()
+
+# Add sprites to group
+all_sprites.add(player)
+all_sprites.add(apple)
+all_sprites.add(strawberry)
+
+# Get the clock
+clock = pygame.time.Clock()
+
+
 # Create the game loop
 running = True
 while running:
-# Look at events
+  # Looks at events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
@@ -89,15 +147,14 @@ while running:
       elif event.key == pygame.K_DOWN:
         player.down()
 
+  # Clear screen
   screen.fill((255, 255, 255))
-  # Draw apple
-  apple.move()
-  apple.render(screen)
-  # Draw player
-  player.move()
-  player.render(screen)
-
+  # Move and render Sprites
+  for entity in all_sprites:
+    entity.move()
+    entity.render(screen)
   # Update the window
   pygame.display.flip()
-  clock.tick(60)
 
+  # tick the clock!
+  clock.tick(30)
